d30ef1218548721cfb0f20ee9656742224b2281d
djkrose <djkrose@live.com>
2017-11-28 17:06:59 +0100

    Some (unsuccessful) tests with sending inventory updates to client

--- ScriptingMod/Commands/Test.cs
+++ ScriptingMod/Commands/Test.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using JetBrains.Annotations;
+using ScriptingMod.Extensions;
 using ScriptingMod.Tools;
 using UnityEngine;
 
@@ -28,7 +29,47 @@ namespace ScriptingMod.Commands
         {
             try
             {
+                //var world = GameManager.Instance.World ?? throw new NullReferenceException(Resources.ErrorWorldNotReady);
+                //var hashSetField = ReflectionTools.GetField(typeof(NetEntityDistribution), "hashSetList_0");
+                //var hashSet = (HashSetList<NetEntityDistributionEntry>)hashSetField.GetValue(world.entityDistributer);
+                //var hashMapField = ReflectionTools.GetField(typeof(NetEntityDistribution), "intHashMap_0");
+                //var hashMap = (IntHashMap)hashMapField.GetValue(world.entityDistributer);
+                //var entryPlayer = hashMap.lookup(senderInfo.RemoteClientInfo.entityId) as NetEntityDistributionEntry;
+                //var entityPlayer = senderInfo.RemoteClientInfo.GetEntityPlayer();
+                //var bagSlots = entityPlayer.bag.GetSlots();
 
+
+                int min = 1;
+                int max = 600;
+                ItemValue itemValue;
+
+                if (int.TryParse(parameters[0], out var itemId))
+                {
+                    itemValue = ItemClass.list[itemId] == null ? ItemValue.None : new ItemValue(itemId, min, max, true);
+                }
+                else
+                {
+                    if (!ItemClass.ItemNames.Contains(parameters[0]))
+                    {
+                        SdtdConsole.Instance.Output($"Unable to find item by id '{parameters[0]}'");
+                        return;
+                    }
+
+                    itemValue = new ItemValue(ItemClass.GetItem(parameters[0]).type, min, max, true);
+                }
+
+                if (Equals(itemValue, ItemValue.None))
+                {
+                    SdtdConsole.Instance.Output($"Unable to find item by name '{parameters[0]}'");
+                    return;
+                }
+
+                EntityPlayer entityPlayer = senderInfo.RemoteClientInfo.GetEntityPlayer();
+                ItemStack itemStack = new ItemStack(itemValue, 1);
+                entityPlayer.bag.SetSlot(30, itemStack);
+                AIDirectorPlayerInventory inventory = AIDirectorPlayerInventory.FromEntity(entityPlayer);
+                NetPackagePlayerInventory package = new NetPackagePlayerInventory(entityPlayer, inventory);
+                senderInfo.RemoteClientInfo.SendPackage(package);
             }
             catch (Exception ex)
             {
