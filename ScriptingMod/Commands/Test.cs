609066b09ee940f1254f96ca2b6deddeaa491971
djkrose <djkrose@live.com>
2017-11-30 11:42:02 +0100

    Added new example script for js-pay; renamed some others

--- ScriptingMod/Commands/Test.cs
+++ ScriptingMod/Commands/Test.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using JetBrains.Annotations;
+using ScriptingMod.Exceptions;
 using ScriptingMod.Extensions;
 using ScriptingMod.Tools;
 using UnityEngine;
@@ -29,47 +29,37 @@ namespace ScriptingMod.Commands
         {
             try
             {
-                //var world = GameManager.Instance.World ?? throw new NullReferenceException(Resources.ErrorWorldNotReady);
-                //var hashSetField = ReflectionTools.GetField(typeof(NetEntityDistribution), "hashSetList_0");
-                //var hashSet = (HashSetList<NetEntityDistributionEntry>)hashSetField.GetValue(world.entityDistributer);
-                //var hashMapField = ReflectionTools.GetField(typeof(NetEntityDistribution), "intHashMap_0");
-                //var hashMap = (IntHashMap)hashMapField.GetValue(world.entityDistributer);
-                //var entryPlayer = hashMap.lookup(senderInfo.RemoteClientInfo.entityId) as NetEntityDistributionEntry;
-                //var entityPlayer = senderInfo.RemoteClientInfo.GetEntityPlayer();
-                //var bagSlots = entityPlayer.bag.GetSlots();
+                var ci = senderInfo.RemoteClientInfo ?? throw new FriendlyMessageException(Resources.ErrorNotRemotePlayer);
+                var world = GameManager.Instance.World ?? throw new FriendlyMessageException(Resources.ErrorWorldNotReady);
+                var player = senderInfo.RemoteClientInfo.GetEntityPlayer();
+                var bounds = BoundsUtils.ExpandBounds(player.boundingBox, 2, 2, 2);
+                var entities = world.GetEntitiesInBounds(typeof(EntityItem), bounds, new List<Entity>());
+                int counter = 0;
 
+                var tt = typeof(EntityItem);
+                Log.Dump(tt);
+                Log.Dump(tt.UnderlyingSystemType);
 
-                int min = 1;
-                int max = 600;
-                ItemValue itemValue;
-
-                if (int.TryParse(parameters[0], out var itemId))
+                foreach (var entity in entities)
                 {
-                    itemValue = ItemClass.list[itemId] == null ? ItemValue.None : new ItemValue(itemId, min, max, true);
+                    // Filter out derived types like EntityBackpack and EntiyLootContainer
+                    if (entity.GetType() != typeof(EntityItem))
+                        continue;
+
+                    var entityItem = (EntityItem) entity;
+                    Log.Out($"Player \"{ci.playerName}\" ({ci.playerId}) paid {entityItem.itemStack.count}x {entityItem.itemStack.itemValue.ItemClass.Name} of quality {entityItem.itemStack.itemValue.Quality}.");
+                    world.RemoveEntity(entity.entityId, EnumRemoveEntityReason.Killed);
+                    counter++;
+                }
+
+                if (counter == 0)
+                {
+                    SdtdConsole.Instance.Output("Could not find any dropped items nearby.");
                 }
                 else
                 {
-                    if (!ItemClass.ItemNames.Contains(parameters[0]))
-                    {
-                        SdtdConsole.Instance.Output($"Unable to find item by id '{parameters[0]}'");
-                        return;
-                    }
-
-                    itemValue = new ItemValue(ItemClass.GetItem(parameters[0]).type, min, max, true);
+                    SdtdConsole.Instance.Output($"Removed {counter} items as payment.");
                 }
-
-                if (Equals(itemValue, ItemValue.None))
-                {
-                    SdtdConsole.Instance.Output($"Unable to find item by name '{parameters[0]}'");
-                    return;
-                }
-
-                EntityPlayer entityPlayer = senderInfo.RemoteClientInfo.GetEntityPlayer();
-                ItemStack itemStack = new ItemStack(itemValue, 1);
-                entityPlayer.bag.SetSlot(30, itemStack);
-                AIDirectorPlayerInventory inventory = AIDirectorPlayerInventory.FromEntity(entityPlayer);
-                NetPackagePlayerInventory package = new NetPackagePlayerInventory(entityPlayer, inventory);
-                senderInfo.RemoteClientInfo.SendPackage(package);
             }
             catch (Exception ex)
             {
